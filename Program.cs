3f907b76d8a428f1fb784f2b09422d2a99ad9ef5
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-04-25 07:52:53 +0530

    added all code files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace stringsplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            var result = str.Split("");
+
+            foreach(var s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
